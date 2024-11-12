16aa83089c7b44715c64e3d4c7b694ea3bfce869
Jay <injaesong0@gmail.com>
2024-11-13 08:16:46 +0900

    feat: MyBatis Audit 인터셉터 구현

--- be/src/main/java/com/mini_ssafy_heaven/global/config/MyBatisConfig.java
+++ be/src/main/java/com/mini_ssafy_heaven/global/config/MyBatisConfig.java
@@ -1,11 +1,49 @@
 package com.mini_ssafy_heaven.global.config;
 
+import com.mini_ssafy_heaven.global.interceptor.MyBatisAuditInterceptor;
+import java.util.Objects;
+import javax.sql.DataSource;
+import lombok.RequiredArgsConstructor;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
+import org.mybatis.spring.boot.autoconfigure.MybatisProperties;
+import org.mybatis.spring.boot.autoconfigure.MybatisProperties.CoreConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Repository;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
 @Configuration
-@MapperScan(basePackages="com.mini_ssafy_heaven.dao", annotationClass=Repository.class)
+@MapperScan("com.mini_ssafy_heaven.dao")
+@RequiredArgsConstructor
 public class MyBatisConfig {
+
+  private final DataSource dataSource;
+  private final MyBatisAuditInterceptor myBatisAuditInterceptor;
+  private final MybatisProperties mybatisProperties;
+
+  @Bean
+  public SqlSessionFactory sqlSessionFactory() throws Exception {
+    SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+    String typeAliasesPackage = mybatisProperties.getTypeAliasesPackage();
+    CoreConfiguration mybatisConfig = mybatisProperties.getConfiguration();
+    String mapperLocation = mybatisProperties.getMapperLocations()[0];
+    Resource[] mapperLocations = new PathMatchingResourcePatternResolver()
+        .getResources(mapperLocation);
+
+    sqlSessionFactoryBean.setDataSource(dataSource);
+    sqlSessionFactoryBean.setPlugins(myBatisAuditInterceptor);
+    sqlSessionFactoryBean.setTypeAliasesPackage(typeAliasesPackage);
+    sqlSessionFactoryBean.setMapperLocations(mapperLocations);
+
+    SqlSessionFactory sqlSessionFactory = sqlSessionFactoryBean.getObject();
+
+    Objects.requireNonNull(sqlSessionFactory)
+        .getConfiguration()
+        .setMapUnderscoreToCamelCase(mybatisConfig.getMapUnderscoreToCamelCase());
+
+    return sqlSessionFactory;
+  }
 
 }
