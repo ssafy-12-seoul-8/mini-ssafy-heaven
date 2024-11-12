2c71764155d58ce6251a0ec53d79e14f0d09f1c1
Jay <injaesong0@gmail.com>
2024-11-13 08:17:15 +0900

    feat: Auto Increment 아이디 예시 구현

---
+++ be/src/main/java/com/mini_ssafy_heaven/dao/RoomDao.java
@@ -0,0 +1,10 @@
+package com.mini_ssafy_heaven.dao;
+
+import com.mini_ssafy_heaven.domain.Room;
+import org.apache.ibatis.annotations.Param;
+
+public interface RoomDao {
+
+  int save(@Param("room") Room room);
+
+}
