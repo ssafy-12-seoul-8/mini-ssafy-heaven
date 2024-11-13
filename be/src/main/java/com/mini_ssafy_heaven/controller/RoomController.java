17b442394b543f76c5d3d999d95f07638c7962ab
송인재 <injaesong0@gmail.com>
2024-11-13 12:05:12 +0900

    feat: 방 생성 컨트롤러 구현

---
+++ be/src/main/java/com/mini_ssafy_heaven/controller/RoomController.java
@@ -0,0 +1,36 @@
+package com.mini_ssafy_heaven.controller;
+
+import java.net.URI;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.mini_ssafy_heaven.doc.RoomDocument;
+import com.mini_ssafy_heaven.dto.request.CreateRoomRequest;
+import com.mini_ssafy_heaven.dto.response.CreateRoomResponse;
+import com.mini_ssafy_heaven.service.RoomService;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/rooms")
+@RequiredArgsConstructor
+public class RoomController implements RoomDocument {
+
+  private final RoomService roomService;
+
+  @Override
+  @PostMapping
+  public ResponseEntity<CreateRoomResponse> create(@RequestBody CreateRoomRequest request,
+      HttpSession session) {
+    // TODO: 로그인 기능 생성 후 실 사용자로 바꾸기
+    Long loginId = 1L;
+    CreateRoomResponse response = roomService.create(request, loginId);
+    URI uri = URI.create("/api/rooms/" + response.id());
+
+    return ResponseEntity.created(uri)
+        .body(response);
+  }
+
+}
