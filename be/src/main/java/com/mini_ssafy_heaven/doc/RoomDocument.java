bcfbfa70aabf935dc6b64c213e360de5a10db57d
송인재 <injaesong0@gmail.com>
2024-11-13 12:04:47 +0900

    docs: 방 생성 문서 추가

---
+++ be/src/main/java/com/mini_ssafy_heaven/doc/RoomDocument.java
@@ -0,0 +1,39 @@
+package com.mini_ssafy_heaven.doc;
+
+import org.springframework.http.ResponseEntity;
+import com.mini_ssafy_heaven.doc.example.RoomErrorExample;
+import com.mini_ssafy_heaven.dto.request.CreateRoomRequest;
+import com.mini_ssafy_heaven.dto.response.CreateRoomResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpSession;
+
+@Tag(name = "방 API", description = "방 관련 API")
+public interface RoomDocument {
+
+  @Operation(summary = "방 생성")
+  @ApiResponses(
+      {
+        @ApiResponse(
+            responseCode = "201",
+            description = "OK"
+        ),
+        @ApiResponse(
+            responseCode = "409",
+            description = "CONFLICT",
+            content = @Content(
+                examples = @ExampleObject(
+                    description = "방장이 다른 방에 참여하고 있을 때",
+                    value = RoomErrorExample.JOINED_PLAYER
+                )
+            )
+        )
+    }
+  )
+  ResponseEntity<CreateRoomResponse> create(CreateRoomRequest request, HttpSession session);
+
+}
---
+++ be/src/main/java/com/mini_ssafy_heaven/doc/example/RoomErrorExample.java
@@ -0,0 +1,11 @@
+package com.mini_ssafy_heaven.doc.example;
+
+public final class RoomErrorExample {
+
+  public static final String JOINED_PLAYER = """
+          {
+            "message" : "다른 방에 참여하는 사용자입니다."
+          }
+          """;
+  
+}
