816e1398fbef1a85adc17f6aa9af1acb8add995e
Jay <injaesong0@gmail.com>
2024-11-16 14:47:11 +0900

    feat: 게임 전체 조회 쿼리 구현

--- be/src/main/java/com/mini_ssafy_heaven/dao/GameDao.java
+++ be/src/main/java/com/mini_ssafy_heaven/dao/GameDao.java
@@ -1,10 +1,13 @@
 package com.mini_ssafy_heaven.dao;
 
+import com.mini_ssafy_heaven.dto.response.GameDetailResponse;
 import java.util.List;
 import java.util.Set;
 
 public interface GameDao {
 
   Set<Long> findIdsIn(List<Long> ids);
+
+  List<GameDetailResponse> findAll();
 
 }
