4c83bd3e337336f0e5a3bc2ffcf710c23fc6d058
Jay <injaesong0@gmail.com>
2024-11-17 12:22:27 +0900

    feat: application.yml에서 클라이언트 주소 주입

--- be/src/main/java/com/mini_ssafy_heaven/BeApplication.java
+++ be/src/main/java/com/mini_ssafy_heaven/BeApplication.java
@@ -2,8 +2,10 @@ package com.mini_ssafy_heaven;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class BeApplication {
 
   public static void main(String[] args) {
