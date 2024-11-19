8a674810bf0ade4bc55c6907f6cd4e0c88d6dfe4
한병현 <gksqudgus777@naver.com>
2024-11-20 08:59:27 +0900

    feat: 공통 컴포넌트 (BaseInput) 구현

---
+++ fe/src/components/BaseInput.vue
@@ -0,0 +1,50 @@
+<template>
+    <div>
+      <div class="flex items-center justify-center">
+        <div class="relative">
+          <input
+            class="border-b border-gray-300 py-1 focus:border-b-2 focus:border-blue-400 transition-colors focus:outline-none peer bg-inherit text-3xl"
+            :id="name"
+            :name="name"
+            :type="type"
+            placeholder=""
+            :value="modelValue"
+            @input="$emit('update:modelValue', $event.target.value)"
+          />
+          <label
+            :for="name"
+            class="absolute -top-4 text-xs left-0 cursor-text peer-focus:text-base peer-focus:-top-4 transition-all peer-focus:text-blue-400 peer-placeholder-shown:top-1 peer-placeholder-shown:text-xl"
+          >
+            {{ label }}
+          </label>
+        </div>
+      </div>
+    </div>
+</template>
+<script setup>
+import { ref, onMounted } from 'vue'
+
+const type = ref('text')
+  const { name, label, password, modelValue } = defineProps({
+    name: {
+      type: String,
+      required: true },
+    label: {
+      type: String,
+      required: true },
+    password: {
+      type: Boolean,
+      default: false },
+    modelValue: {
+      type: String,
+      required: false,
+      default: ''},    
+  });
+
+  onMounted(() => {
+    if (password) {
+        type.value = 'password';
+    }
+  });
+</script>
+  
