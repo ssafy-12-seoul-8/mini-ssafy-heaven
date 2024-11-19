39f8cd9cfd9ddaf1122a1ffbdf7261bef4e55360
한병현 <gksqudgus777@naver.com>
2024-11-19 09:46:42 +0900

    feat: 회원가입 Controller 구현

--- be/src/main/java/com/mini_ssafy_heaven/controller/MemberController.java
+++ be/src/main/java/com/mini_ssafy_heaven/controller/MemberController.java
@@ -1,38 +1,34 @@
 package com.mini_ssafy_heaven.controller;
 
+import com.mini_ssafy_heaven.doc.MemberDocument;
+import com.mini_ssafy_heaven.dto.request.CreateMemberRequest;
+import com.mini_ssafy_heaven.dto.response.CreateMemberResponse;
+import com.mini_ssafy_heaven.service.MemberService;
 import java.net.URI;
-
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.mini_ssafy_heaven.doc.MemberDocument;
-import com.mini_ssafy_heaven.dto.request.CreateMemberRequest;
-import com.mini_ssafy_heaven.dto.response.CreateMemberResponse;
-import com.mini_ssafy_heaven.service.MemberService;
-
-import jakarta.servlet.http.HttpSession;
-import lombok.RequiredArgsConstructor;
-
 @RestController
 @RequestMapping("/api/members")
 @RequiredArgsConstructor
-public class MemberController implements MemberDocument{
-	
+public class MemberController implements MemberDocument {
+
   private final MemberService memberService;
-  
+
   @Override
   @PostMapping
-  public ResponseEntity<CreateMemberResponse> addMember(@RequestBody CreateMemberRequest request,
-		  HttpSession session){
-    
-	CreateMemberResponse response = memberService.addMember(request);
-	URI uri = URI.create("/api/members/" + response.id());
-	
-	return ResponseEntity.created(uri)
-			.body(response);
-	
+  public ResponseEntity<CreateMemberResponse> addMember(@RequestBody
+  CreateMemberRequest request) {
+
+    CreateMemberResponse response = memberService.addMember(request);
+    URI uri = URI.create("/api/members/" + response.id());
+
+    return ResponseEntity.created(uri)
+        .body(response);
   }
+
 }
--- be/src/main/java/com/mini_ssafy_heaven/doc/MemberDocument.java
+++ be/src/main/java/com/mini_ssafy_heaven/doc/MemberDocument.java
@@ -1,41 +1,33 @@
 package com.mini_ssafy_heaven.doc;
 
-import org.springframework.http.ResponseEntity;
-
-import com.mini_ssafy_heaven.doc.example.RoomErrorExample;
+import com.mini_ssafy_heaven.doc.example.MemberErrorExample;
 import com.mini_ssafy_heaven.dto.request.CreateMemberRequest;
 import com.mini_ssafy_heaven.dto.response.CreateMemberResponse;
-
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.servlet.http.HttpSession;
+import org.springframework.http.ResponseEntity;
 
 @Tag(name = "회원 API", description = "회원 관련 API")
 public interface MemberDocument {
 
   @Operation(summary = "회원 생성")
   @ApiResponses(
-      {
+    {@ApiResponse(responseCode = "201", description = "OK"),
         @ApiResponse(
-            responseCode = "201",
-            description = "OK"
-        ),
-        @ApiResponse(
-            responseCode = "409",
-            description = "CONFLICT",
-            content = @Content(
-                examples = @ExampleObject(
-                    description = "방장이 다른 방에 참여하고 있을 때",
-                    value = RoomErrorExample.JOINED_PLAYER
-                )
+          responseCode = "409",
+          description = "CONFLICT",
+          content = @Content(
+            examples = @ExampleObject(
+              description = "이미 존재하는 회원일 때",
+              value = MemberErrorExample.EXISTING_MEMBER
             )
-        )
-    }
+          )
+        )}
   )
-  ResponseEntity<CreateMemberResponse> addMember(CreateMemberRequest request, HttpSession session);
+  ResponseEntity<CreateMemberResponse> addMember(CreateMemberRequest request);
 
 }
