3ffa48ac6a9ee3ccab759d6e7de4cafce7dd2f04
한병현 <gksqudgus777@naver.com>
2024-11-20 08:58:58 +0900

    feat: 공통 컴포넌트 (BaseButton) 구현

---
+++ fe/src/components/BaseButton.vue
@@ -0,0 +1,9 @@
+<template>
+  <button class="cursor-pointer transition-all bg-blue-400 text-white px-6 py-2 rounded-lg
+border-blue-500
+border-b-[4px] hover:brightness-110 hover:-translate-y-[1px] hover:border-b-[6px]
+active:border-b-[2px] active:brightness-90 active:translate-y-[2px]">
+  가입하기
+  </button>
+
+</template>
