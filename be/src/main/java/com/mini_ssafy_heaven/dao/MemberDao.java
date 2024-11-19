017af2de1f305c83ca629789e09de6009d48ad8d
한병현 <gksqudgus777@naver.com>
2024-11-19 09:45:13 +0900

    feat: 회원 Dao 구현

--- be/src/main/java/com/mini_ssafy_heaven/dao/MemberDao.java
+++ be/src/main/java/com/mini_ssafy_heaven/dao/MemberDao.java
@@ -1,11 +1,14 @@
 package com.mini_ssafy_heaven.dao;
 
-import org.apache.ibatis.annotations.Param;
-
 import com.mini_ssafy_heaven.domain.Member;
+import org.apache.ibatis.annotations.Param;
 
 public interface MemberDao {
 
-  boolean existsById(String username);
-  int addMember(@Param("member") Member member);
+  boolean existsById(Long id);
+
+  boolean existsByUserName(String username);
+
+  int addMember(@Param("member")
+  Member member);
 }
