618d6e8918db7ee98716bfa4cb84aabda81fbd74
송인재 <injaesong0@gmail.com>
2024-11-20 17:08:57 +0900

    feat: 웹소켓 메세지 형식 정형화

--- be/src/main/java/com/mini_ssafy_heaven/dto/response/MessageResponse.java
+++ be/src/main/java/com/mini_ssafy_heaven/dto/response/MessageResponse.java
@@ -2,6 +2,10 @@ package com.mini_ssafy_heaven.dto.response;
 
 import com.mini_ssafy_heaven.domain.enums.SocketMessageType;
 
-public record MessageResponse(SocketMessageType type, String message) {
+public record MessageResponse<T>(SocketMessageType type, T payload) {
+
+  public static <T> MessageResponse<T> from(SocketMessageType type, T payload) {
+    return new MessageResponse<T>(type, payload);
+  }
 
 }
