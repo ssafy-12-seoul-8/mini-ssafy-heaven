3dabe463a0f27cc3b7b6d182f5a8d0a31d88d875
송인재 <injaesong0@gmail.com>
2024-11-20 13:10:45 +0900

    remove: 소켓 관련 객체 삭제

--- be/src/main/java/com/mini_ssafy_heaven/dao/PlayerCountRepository.java
+++
@@ -1,8 +0,0 @@
-package com.mini_ssafy_heaven.dao;
-
-import com.mini_ssafy_heaven.domain.RoomPlayerCount;
-import org.springframework.data.repository.CrudRepository;
-
-public interface PlayerCountRepository extends CrudRepository<RoomPlayerCount, Long> {
-
-}
