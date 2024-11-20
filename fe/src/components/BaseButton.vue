a3994f02c01b6b90d255557573e129214a7a175f
한병현 <gksqudgus777@naver.com>
2024-11-20 11:26:16 +0900

    pref: 공통 컴포넌트 (button) 작동시 높이 고정

--- fe/src/components/BaseButton.vue
+++ fe/src/components/BaseButton.vue
@@ -1,9 +1,25 @@
 <template>
-  <button class="cursor-pointer transition-all bg-blue-400 text-white px-6 py-2 rounded-lg
-border-blue-500
-border-b-[4px] hover:brightness-110 hover:-translate-y-[1px] hover:border-b-[6px]
-active:border-b-[2px] active:brightness-90 active:translate-y-[2px]">
-  가입하기
+  <button :class="[
+      'cursor-pointer transition-all px-6 py-2 rounded-lg border-b-[4px] hover:brightness-110 hover:-translate-y-[1px] hover:border-b-[6px] active:border-b-[2px] active:brightness-90 active:translate-y-[2px]',
+      buttonColor
+    ]" :style="{ height: '50px' }">
+    <slot>가입하기</slot>
   </button>
-
 </template>
+<script setup>
+import { defineProps, computed } from 'vue';
+
+const props = defineProps({
+  type: {
+    type: String,
+    default: 'blue',
+  }
+})
+// white로 주면 색 변경
+const buttonColor = computed(() => {
+  if (props.type === 'white') {
+    return 'bg-white text-blue-400';
+  }
+  return 'bg-blue-400 text-white border-blue-500'
+})
+</script>
