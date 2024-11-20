68f3cd82e3759cd460ba39d60b9b88ef3219755f
송인재 <injaesong0@gmail.com>
2024-11-20 14:57:29 +0900

    feat: 방장 확인용 방 플레이어 조회 쿼리 구현

--- be/src/main/java/com/mini_ssafy_heaven/dao/RoomPlayerDao.java
+++ be/src/main/java/com/mini_ssafy_heaven/dao/RoomPlayerDao.java
@@ -1,6 +1,7 @@
 package com.mini_ssafy_heaven.dao;
 
 import com.mini_ssafy_heaven.domain.RoomPlayer;
+import java.util.Optional;
 import org.apache.ibatis.annotations.Param;
 
 public interface RoomPlayerDao {
@@ -10,5 +11,10 @@ public interface RoomPlayerDao {
   int save(@Param("roomPlayer") RoomPlayer roomPlayer);
 
   int countByRoomId(Long roomId);
+
+  Optional<RoomPlayer> findByRoomAndMember(
+    @Param("roomId") Long roomId,
+    @Param("memberId") Long memberId
+  );
 
 }
