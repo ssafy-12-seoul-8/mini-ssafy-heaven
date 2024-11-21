fa9a463fbeafdba384aa3072c581d5930cb93804
한병현 <gksqudgus777@naver.com>
2024-11-21 15:23:02 +0900

    perf: 공통 컴포넌트 디자인 개선

--- fe/src/components/BaseButton.vue
+++ fe/src/components/BaseButton.vue
@@ -1,25 +1,35 @@
 <template>
-  <button :class="[
-      'cursor-pointer transition-all px-6 py-2 rounded-lg border-b-[4px] hover:brightness-110 hover:-translate-y-[1px] hover:border-b-[6px] active:border-b-[2px] active:brightness-90 active:translate-y-[2px]',
-      buttonColor
-    ]" :style="{ height: '50px' }">
-    <slot>가입하기</slot>
+  <button
+    class="'cursor-pointer transition-all px-6 py-2 rounded-lg border-b-[4px] w-48 hover:brightness-110 hover:-translate-y-[1px] active:border-b-[2px] active:brightness-90 active:translate-y-[2px]'"
+    :class="[buttonColor, buttonSize]"
+    :style="{ height: '50px' }"
+  >
+    <slot></slot>
   </button>
 </template>
 <script setup>
-import { defineProps, computed } from 'vue';
+import { defineProps, computed } from 'vue'
 
 const props = defineProps({
   type: {
     type: String,
     default: 'blue',
-  }
+  },
+  size: {
+    type: String,
+    default: 'w-48'
+  },
 })
 // white로 주면 색 변경
 const buttonColor = computed(() => {
   if (props.type === 'white') {
-    return 'bg-white text-blue-400';
+    return 'bg-white text-sky-400'
   }
-  return 'bg-blue-400 text-white border-blue-500'
+  return 'bg-sky-400 text-white border-sky-500'
+})
+// size 입력으로 너비 조절
+const buttonSize = computed(() => {
+  const validSizes = ["w-24", "w-32", "w-48", "w-64"]; // 유효한 너비
+  return validSizes.includes(props.size) ? props.size : "w-48";
 })
 </script>
