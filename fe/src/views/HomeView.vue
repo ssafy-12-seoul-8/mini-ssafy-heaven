20a19c7b90ca74f07f5076142078ad967aa4be51
한병현 <gksqudgus777@naver.com>
2024-11-21 15:30:51 +0900

    perf: 메인 페이지 개선(로그인 페이지 이동, 스타일 개선)

--- fe/src/views/HomeView.vue
+++ fe/src/views/HomeView.vue
@@ -1,11 +1,20 @@
 <template>
-  <div class="flex flex-col">
+  <div class="flex flex-col items-center gap-40">
     <BaseLogo></BaseLogo>
-    <BaseButton>입장하기</BaseButton>
+    <div class="gap-20">
+      <BaseButton @click="login">입장하기</BaseButton>
+    </div>
   </div>
 </template>
 
 <script setup>
 import BaseButton from '@/components/BaseButton.vue'
 import BaseLogo from '@/components/BaseLogo.vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+const login = () => {
+  router.push({ path: '/login' })
+}
 </script>
