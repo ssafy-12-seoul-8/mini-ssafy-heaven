542b6056fb41a56bc45dc91b7b03cab732452611
송인재 <injaesong0@gmail.com>
2024-11-21 16:21:26 +0900

    feat: 방 상세페이지 라우터 뷰 구현

--- fe/src/views/RoomView.vue
+++ fe/src/views/RoomView.vue
@@ -1,11 +1,24 @@
 <template>
-  <div>{{ roomId }}번 방 입장</div>
+  <div class="h-full w-full">
+    <ChatBox />
+    <div id="player-list-container-wrapper" class="w-screen h-5/6 absolute left-0">
+      <PlayerList :room-players="dummyPlayers" />
+    </div>
+  </div>
 </template>
 
 <script setup>
-import { ref } from 'vue'
-import { useRoute } from 'vue-router'
+import PlayerList from '@/components/PlayerList.vue'
+import ChatBox from '@/components/ChatBox.vue'
+import { useRoomPlayerStore } from '@/stores/roomPlayers'
+import { storeToRefs } from 'pinia'
 
-const { params } = useRoute()
-const roomId = ref(params.id)
+const roomPlayerStore = useRoomPlayerStore()
+const { dummyPlayers } = storeToRefs(roomPlayerStore)
 </script>
+
+<style>
+#player-list-container-wrapper {
+  top: 8%;
+}
+</style>
