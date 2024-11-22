940221d82004d95695cdedf2f170453454f6a48f
송인재 <injaesong0@gmail.com>
2024-11-22 14:18:50 +0900

    feat: 게스트 로그인 컨트롤러 구현

--- be/src/main/java/com/mini_ssafy_heaven/controller/MemberController.java
+++ be/src/main/java/com/mini_ssafy_heaven/controller/MemberController.java
@@ -3,7 +3,9 @@ package com.mini_ssafy_heaven.controller;
 import com.mini_ssafy_heaven.doc.MemberDocument;
 import com.mini_ssafy_heaven.dto.request.CreateMemberRequest;
 import com.mini_ssafy_heaven.dto.response.CreateMemberResponse;
+import com.mini_ssafy_heaven.dto.response.GuestLoginResponse;
 import com.mini_ssafy_heaven.service.MemberService;
+import jakarta.servlet.http.HttpSession;
 import java.net.URI;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -21,11 +23,23 @@ public class MemberController implements MemberDocument {
 
   @Override
   @PostMapping
-  public ResponseEntity<CreateMemberResponse> addMember(@RequestBody
-  CreateMemberRequest request) {
+  public ResponseEntity<CreateMemberResponse> addMember(@RequestBody CreateMemberRequest request) {
 
     CreateMemberResponse response = memberService.addMember(request);
     URI uri = URI.create("/api/members/" + response.id());
+
+    return ResponseEntity.created(uri)
+        .body(response);
+  }
+
+  @Override
+  @PostMapping("/login/guest")
+  public ResponseEntity<GuestLoginResponse> loginGuest(HttpSession session) {
+    GuestLoginResponse response = memberService.loginGuest();
+    URI uri = URI.create("/api/members/" + response.id());
+
+    session.setAttribute("loginId", response.id());
+    session.setMaxInactiveInterval(60 * 60 * 24);
 
     return ResponseEntity.created(uri)
         .body(response);
