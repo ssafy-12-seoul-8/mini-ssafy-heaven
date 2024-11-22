4258bb87c1fc23e2c6d34dc22a0689952df89f4b
Jay <injaesong0@gmail.com>
2024-11-22 11:58:16 +0900

    feat: 준비 상태에 따른 플레이어 전역 상태 변경 추가

--- fe/src/stores/roomPlayers.js
+++ fe/src/stores/roomPlayers.js
@@ -12,14 +12,27 @@ export const useRoomPlayerStore = defineStore('roomPlayer', () => {
     return sorted.reverse()
   })
 
+  // TODO: 회원 구현 후 수정
+  const currentPlayer = computed(() => roomPlayers.value.find((player) => player.memberId === 1))
+  const manager = computed(() => roomPlayers.value[0])
+
   // actions
   const updatePlayers = (players) => {
     roomPlayers.value = players
   }
 
+  const updateReadyPlayer = (memberId, status) => {
+    const player = roomPlayers.value.find((player) => player.memberId === memberId)
+
+    player.status = status
+  }
+
   return {
     roomPlayers,
     sortedByRank,
+    currentPlayer,
+    manager,
     updatePlayers,
+    updateReadyPlayer,
   }
 })
