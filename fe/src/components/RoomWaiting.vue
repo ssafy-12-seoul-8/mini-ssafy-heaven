c9bbff55fd8d478a60a0f5462cff1a71627ed98c
Jay <injaesong0@gmail.com>
2024-11-22 11:59:44 +0900

    feat: 방 컨텐츠 컴포넌트 구현

---
+++ fe/src/components/RoomWaiting.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="flex flex-col gap-4 justify-center items-center">
+    <span class="text-3xl">시작 대기 중입니다...</span>
+    <span class="text-xl"
+      >준비 인원: {{ currentRoom.currentReadyCount }} / {{ currentRoom.totalCount }}</span
+    >
+  </div>
+</template>
+
+<script setup>
+import { useRoomStore } from '@/stores/rooms'
+import { storeToRefs } from 'pinia'
+
+const roomStore = useRoomStore()
+const { currentRoom } = storeToRefs(roomStore)
+</script>
