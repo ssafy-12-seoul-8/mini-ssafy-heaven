cc304cf2310ab05974ea692046dd76d507cc3fca
Jay <injaesong0@gmail.com>
2024-11-22 11:47:24 +0900

    fix: 방 상세조회 시 응답에 플레이어 상태 추가

--- be/src/main/java/com/mini_ssafy_heaven/dto/query/RoomPlayerNameDto.java
+++ be/src/main/java/com/mini_ssafy_heaven/dto/query/RoomPlayerNameDto.java
@@ -1,11 +1,16 @@
 package com.mini_ssafy_heaven.dto.query;
 
+import com.mini_ssafy_heaven.domain.enums.RoomPlayerStatus;
+import lombok.Builder;
+
+@Builder
 public record RoomPlayerNameDto(
-  Long memberId,
-  String nickname,
-  Integer roomScore,
-  Integer score,
-  Integer rank
+    Long memberId,
+    String nickname,
+    Integer roomScore,
+    Integer score,
+    Integer rank,
+    RoomPlayerStatus status
 ) {
 
 }
