407a2103a3d9d65714f16c844b5233216c62186c
Jay <injaesong0@gmail.com>
2024-11-23 09:54:43 +0900

    style: 채팅 스크롤 적용

--- fe/src/components/ChatBox.vue
+++ fe/src/components/ChatBox.vue
@@ -4,7 +4,10 @@
       id="chat-box-wrapper"
       class="flex flex-col justify-center h-full items-center bg-slate-400 mx-auto mb-4 rounded-lg gap-2"
     >
-      <div id="chat-contents-container" class="h-5/6 gap-1 w-11/12 flex flex-col-reverse">
+      <div
+        id="chat-contents-container"
+        class="z-50 h-5/6 gap-1 w-11/12 flex flex-col-reverse overflow-y-auto overflow-x-hidden [&::-webkit-scrollbar]:w-1 [&::-webkit-scrollbar-track]:rounded-full [&::-webkit-scrollbar-track]:bg-gray-100 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-thumb]:bg-gray-300"
+      >
         <div id="chat-contents" v-for="(chat, index) in chats" :key="index">
           {{ chat }}
         </div>
@@ -43,6 +46,10 @@ const { chats } = storeToRefs(chatStore)
 const { currentRoom } = storeToRefs(roomStore)
 
 const sendChat = () => {
+  if (!text.value) {
+    return
+  }
+
   const request = {
     nickname: player.nickname,
     chat: text.value,
