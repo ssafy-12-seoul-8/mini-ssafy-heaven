af0ef1019db78abe305314a8b958232b3dcd02b2
한병현 <gksqudgus777@naver.com>
2024-11-23 15:20:50 +0900

    feat: 방 목록 임시 페이지 구현-FE

--- fe/src/views/RoomsView.vue
+++ fe/src/views/RoomsView.vue
@@ -1,3 +1,14 @@
 <template>
   <h1>rooms</h1>
+  <BaseButton @click="me">내 정보</BaseButton>
 </template>
+<script setup>
+import BaseButton from '@/components/BaseButton.vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+const me = () => {
+    router.push({ path: 'me' })
+}
+</script>
