e23b4b01db912e4c394c1c3531e9ee1195be132b
Jay <injaesong0@gmail.com>
2024-11-24 13:43:41 +0900

    feat: 방 시작 후 컴포넌트

--- fe/src/components/RoomContents.vue
+++ fe/src/components/RoomContents.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="grow flex justify-center items-center">
     <RoomWaiting v-if="isWaiting" />
+    <RoomPlaying v-if="isPlaying" />
   </div>
 </template>
 
@@ -10,8 +11,9 @@ import { useRoomStore } from '@/stores/rooms'
 import RoomWaiting from './RoomWaiting.vue'
 import { storeToRefs } from 'pinia'
 import { RoomStatus } from '@/constants/RoomStatus'
+import RoomPlaying from './RoomPlaying.vue'
 
 const roomStore = useRoomStore()
-const { currentRoom } = storeToRefs(roomStore)
+const { currentRoom, isPlaying } = storeToRefs(roomStore)
 const isWaiting = computed(() => RoomStatus.isWaiting(currentRoom.value.status))
 </script>
---
+++ fe/src/components/RoomPlaying.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex flex-col gap-4 justify-center items-center">
+    {{ content }}
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const content = ref('게임을 시작합니다!')
+</script>
