e6e5543d931d16ab699d27d597db1e9e2efeafb2
Jay <injaesong0@gmail.com>
2024-11-24 11:27:40 +0900

    feat: 방 상태 인게임 추가 및 메세지 찾기 구현

--- fe/src/constants/RoomStatus.js
+++ fe/src/constants/RoomStatus.js
@@ -9,19 +9,33 @@ export const RoomStatus = {
   },
   CREATING: {
     name: 'CREATING',
-    message: '생성 중 . . .',
+    message: '생성 중',
   },
   ENTERING: {
     name: 'ENTERING',
-    message: '입장 중 . . .',
+    message: '입장 중',
   },
   WAITING: {
     name: 'WAITING',
     message: '대기 중',
   },
+  IN_GAME: {
+    name: 'IN_GAME',
+    message: '게임 중',
+  },
   isInput: (name) => name === RoomStatus.INPUT_INFO.name,
   isReady: (name) => name === RoomStatus.READY_FOR_GAME_SELECTION.name,
   isWaiting: (name) => name === RoomStatus.WAITING.name,
+  findMessage: (name) =>
+    Object.entries(RoomStatus).find((entry) => {
+      const status = entry[1]
+
+      if (typeof status !== 'object') {
+        return false
+      }
+
+      return status.name === name
+    })[1].message,
 }
 
 Object.freeze(RoomStatus)
