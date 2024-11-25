f60284a93048a6f6e191023d8d006135f1979bce
Jay <injaesong0@gmail.com>
2024-11-25 21:41:54 +0900

    feat: 게임 상세조회 쿼리 구현

--- be/src/main/java/com/mini_ssafy_heaven/dao/GameDao.java
+++ be/src/main/java/com/mini_ssafy_heaven/dao/GameDao.java
@@ -2,6 +2,7 @@ package com.mini_ssafy_heaven.dao;
 
 import com.mini_ssafy_heaven.dto.response.GameDetailResponse;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 
 public interface GameDao {
@@ -9,5 +10,7 @@ public interface GameDao {
   Set<Long> findIdsIn(List<Long> ids);
 
   List<GameDetailResponse> findAll();
+
+  Optional<GameDetailResponse> findById(Long id);
 
 }
