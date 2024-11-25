2976cefe2245a6a4768af382973823d0da82de4b
Jay <injaesong0@gmail.com>
2024-11-25 17:17:45 +0900

    feat: 숫자야구 레디스 레퍼지터리 추가

---
+++ be/src/main/java/com/mini_ssafy_heaven/repository/BaseballConditionRepository.java
@@ -0,0 +1,8 @@
+package com.mini_ssafy_heaven.repository;
+
+import com.mini_ssafy_heaven.domain.BaseballCondition;
+import org.springframework.data.repository.CrudRepository;
+
+public interface BaseballConditionRepository extends CrudRepository<BaseballCondition, Long> {
+
+}
