85e1f31d6464ee3fe3c89ce2ad05111c3b959fb2
Jay <injaesong0@gmail.com>
2024-11-25 17:17:33 +0900

    feat: 숫자야구 캐싱 정보 추가

---
+++ be/src/main/java/com/mini_ssafy_heaven/domain/BaseballCondition.java
@@ -0,0 +1,31 @@
+package com.mini_ssafy_heaven.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+
+@RedisHash("BASEBALL")
+@Builder
+@Getter
+public class BaseballCondition {
+
+  @Id
+  private final Long roomId;
+  private final String currentBall;
+  private final int ballCount;
+  private final int maxCount;
+  private final Long taggerId;
+
+  @Builder
+  private BaseballCondition(
+      Long roomId, String currentBall, int ballCount, int maxCount, Long taggerId
+  ) {
+    this.roomId = roomId;
+    this.currentBall = currentBall;
+    this.ballCount = ballCount;
+    this.maxCount = maxCount;
+    this.taggerId = taggerId;
+  }
+
+}
