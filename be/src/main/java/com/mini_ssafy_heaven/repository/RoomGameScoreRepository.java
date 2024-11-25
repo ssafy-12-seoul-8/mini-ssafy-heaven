46022c3b3cf23c5f640920e8d007219cfbe5abc5
Jay <injaesong0@gmail.com>
2024-11-25 17:19:11 +0900

    feat: 방 게임 별 점수 캐싱 레디스 추가

---
+++ be/src/main/java/com/mini_ssafy_heaven/repository/RoomGameScoreRepository.java
@@ -0,0 +1,8 @@
+package com.mini_ssafy_heaven.repository;
+
+import com.mini_ssafy_heaven.domain.RoomGameScore;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RoomGameScoreRepository extends CrudRepository<RoomGameScore, String> {
+
+}
